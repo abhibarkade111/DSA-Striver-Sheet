61210e7bd1bdb616f5dbc38bc7637abf71b02376
Abhishek Barkade <abhibarkade111@gmail.com>
2022-06-20 22:56:34 +0530

    Day 10

--- Day 10/Majority Element -II.java
+++ Day 10/Majority Element -II.java
@@ -1,7 +1,11 @@
 // Majority Element-II 
 // Find the element which is occurs more than N/3 times
 
-// Now only hashMap Solution is uploaded remaining approaches such as moore's voting algorithm uploaded soon ......
+// Approach 1: Brute Force 
+// Time Complexity - O(n*n)
+// Space Complexity - O(1)
+
+// Approach 2: HashMap
 
 public class Solution 
 {
@@ -21,3 +25,56 @@ public class Solution
         return ans;
     }
 }
+
+// Time Complexity - O(nlogn)
+// Space Coplexity  - O(n)
+
+// Approch 3: Moore's Voting Algorithm
+// Only maximum two majority(more than n/3) elements is present in array
+
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        ArrayList<Integer> ans =  new ArrayList<>();
+        int num1=Integer.MIN_VALUE,num2=Integer.MIN_VALUE,c1=0,c2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==num1){
+                c1++;
+            }
+            else if(nums[i]==num2){
+                c2++;
+            }
+            else if(c1==0){
+                num1 = nums[i];
+                c1++;
+            }
+            else if(c2==0){
+                num2 = nums[i];
+                c2++;
+            }
+            else{
+                c1--;
+                c2--;
+            }
+        }
+        c1=0;
+        c2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==num1){
+                c1++;
+            }
+            if(nums[i]==num2){
+                c2++;
+            }
+        }
+        if(c1>nums.length/3){
+            ans.add(num1);
+        }
+        if(c2>nums.length/3){
+            ans.add(num2);
+        }
+        return ans;
+    }
+}
+
+// Time Complexity - O(n)
+// Space Complexity - O(1)
