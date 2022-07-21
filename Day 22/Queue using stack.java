e1d96a765232cf0d7313858b78c7b2778f5bf7e1
Abhishek Barkade <abhibarkade111@gmail.com>
2022-07-21 17:08:58 +0530

    Day 22 21-07-22

--- Day 22/Queue using stack.java
+++ Day 22/Queue using stack.java
@@ -1,6 +1,51 @@
 // Queue using Stack 
 
-// Code:
+// Code 1:
+// Optimised
+
+import java.util.*;
+public class Queue {
+    // Define the data members(if any) here.
+    Stack<Integer> st1 = new Stack<>();
+    Stack<Integer> st2 = new Stack<>();
+    Queue() {
+        // Initialize your data structure here.
+    }
+
+    void enQueue(int val) {
+        // Implement the enqueue() function.
+        st1.push(val);
+    }
+
+    int deQueue() {
+        // Implement the dequeue() function.
+        if(st2.isEmpty()){
+            while(!st1.isEmpty()){
+                st2.push(st1.pop());
+            }
+            return st2.isEmpty()?-1:st2.pop();
+        }
+        return st2.isEmpty()?-1:st2.pop();
+    }
+
+    int peek() {
+        // Implement the peek() function here.
+        if(st2.isEmpty()){
+            while(!st1.isEmpty()){
+                st2.push(st1.pop());
+            }
+        }
+        return st2.isEmpty()?-1:st2.peek();
+    }
+
+    boolean isEmpty() {
+        // Implement the isEmpty() function here.
+        return st1.isEmpty()&&st2.isEmpty();
+    }
+}
+
+
+// Code 2:
 
 public class Queue {
     // Define the data members(if any) here.
