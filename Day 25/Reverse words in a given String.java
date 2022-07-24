675ff667e060b1c527ba9d7f97c8fd6e939800cc
Abhishek Barkade <abhibarkade111@gmail.com>
2022-07-24 12:23:49 +0530

    Day 25 24-07-22

---
+++ Day 25/Reverse words in a given String.java
@@ -0,0 +1,78 @@
+// Reverse the word in given string
+// Input: s = "the sky is blue"
+// Output: "blue is sky the"
+
+// Approach 1: Brute Force
+// Traverse the string and find the frist char and then the find the end upto space and add string in the st
+// again pop element from the stack
+
+// Time Complexity -O(n)
+// Space Complexity - O(n)
+
+// Code:
+
+class Solution {
+    public String reverseWords(String s) {
+        Stack<String> st = new Stack<>();
+        int start=0,end=0,i=0;
+        while(i<s.length()){
+            while(i<s.length() && s.charAt(i)==' ') i++;
+            start=i;
+            while(i<s.length() && s.charAt(i)!=' ') i++;
+            end=i;
+            st.push(s.substring(start,end));
+        }
+        String ans="";
+        while(!st.isEmpty()){
+            ans+= st.pop()+" ";
+        }
+        return ans.trim();
+    }
+}
+
+// Approach 2: O(1) space
+
+// Time Complexity - O(n)
+// Space Complexity - O(1)
+
+// Code:
+
+class Solution {
+    public String reverseWords(String s) {
+        int start=0,end=0,i=0;
+        String ans="";
+        while(i<s.length()){
+            while(i<s.length() && s.charAt(i)==' ') i++;
+            start=i;
+            while(i<s.length() && s.charAt(i)!=' ') i++;
+            end=i;
+            ans= s.substring(start,end)+" "+ans;
+        }
+        
+        
+        return ans.trim();
+    }
+}
+
+
+// Approach 3: Using Inbuilt method split
+
+// Time Complexity- O(n)
+// Space Complexity- O(n)
+
+// Code:
+
+class Solution {
+    public String reverseWords(String s) {
+        int start=0,end=0,i=0;
+        String ans="";
+        String arr[] = s.split("\\s+");
+        for(String str : arr){
+            String a = str.trim();
+            if(!a.equals(" "))ans= a+" "+ ans;
+            ans.trim();
+        }
+        
+        return ans.trim();
+    }
+}
