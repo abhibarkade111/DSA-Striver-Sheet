66d6c345a1e002d4407c90cc336f62d971007042
Abhishek Barkade <abhibarkade111@gmail.com>
2022-07-28 16:52:06 +0530

    Day 29 28-07-22

---
+++ Day 29/Morris Preorder Traversal.java
@@ -0,0 +1,54 @@
+// Morris Preorder Traversal 
+// See the below code to the understand
+
+// Time Complexity - O(n)
+// Space Complexity - O(1)
+
+// Code :
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        
+        List<Integer> ans = new ArrayList<>();
+        TreeNode curr = root;
+        while(curr!=null){
+            if(curr.left==null){
+                ans.add(curr.val);
+                curr = curr.right;
+            }
+            else{
+                TreeNode prev = curr.left;
+                while(prev.right!=null && prev.right!=curr){
+                    prev = prev.right;
+                }
+                if(prev.right==null){
+                    ans.add(curr.val);
+                    prev.right = curr;
+                    curr = curr.left;
+                }
+                else{
+                    prev.right = null;
+                    curr = curr.right;
+                }
+            }
+        }
+        return ans;
+        
+        
+    }
+}
