8d59caadde7b9a0a8a7ce5427276d91c5ee3e727
Abhishek Barkade <abhibarkade111@gmail.com>
2022-07-30 23:46:48 +0530

    Day 30 30-07-22

--- Day 27/KMP Algorithm.java
+++ Day 27/KMP Algorithm.java
@@ -37,9 +37,9 @@ public class Solution {
                 return true;
 //                 j = lps[j-1];
             }a
-            else if(i<n && p.charAt(j)!=s.charAt(i)){
+            else if(i<n && p.charAt(j) !=s.charAt(i)){
                 if(j==0){
-                    i++;
+                    i++; 
                 }
                 else{
                     j =lps[j-1];
